1e6e002ce5951ff328ca12483563a1ab6984a985
ayu0616 <ayumuogawa0616@gmail.com>
2022-08-23 10:44:57 +0900

    メールではなくDISCORDで送信するように変更

---
+++ discord.py
@@ -0,0 +1,12 @@
+import requests
+from settings import DISCORD_WEBHOOK_URL
+
+
+def send_to_discord(message: str):
+    payload = {"content": message}
+    if DISCORD_WEBHOOK_URL:
+        res = requests.post(DISCORD_WEBHOOK_URL, data=payload)
+    else:
+        raise Exception("DISCORDのURLが設定されていません")
+    if not res.ok:
+        print("DISCORDへの送信に失敗しました")
